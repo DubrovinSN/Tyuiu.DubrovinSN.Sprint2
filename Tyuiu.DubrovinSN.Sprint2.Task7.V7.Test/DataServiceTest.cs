ec02313feff5156da32ae36e52c35a6cdadbf7cd
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-10-13 22:42:32 +0500

    Task7

---
+++ Tyuiu.DubrovinSN.Sprint2.Task7.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DubrovinSN.Sprint2.Task7.V7.Lib;
+
+namespace Tyuiu.DubrovinSN.Sprint2.Task7.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 1.3;
+            double y = 0.1;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DubrovinSN.Sprint2.Task7.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DubrovinSN.Sprint2.Task7.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DubrovinSN.Sprint2.Task7.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a819e681-69f8-436d-81df-aadae0f90c5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
